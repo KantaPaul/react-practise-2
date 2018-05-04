f6190ee200180f0689fc421be5328bba18ea117f
Kanta Paul <pobon.paul1994@gmail.com>
2018-05-05 01:28:38 +0600

    file add day three

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -9,6 +9,18 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 console.log('app js is running');
+
+var obj = {
+  name: 'Vikram',
+  getName: function getName() {
+    return this.name;
+  }
+};
+
+// let getName = obj.getName.bind(obj);
+var getName = obj.getName.bind({ name: 'ABC' });
+
+console.log(getName());
 
 // 026 Creating a React Component
 
@@ -22,19 +34,19 @@ var Header = function (_React$Component) {
   }
 
   _createClass(Header, [{
-    key: "render",
+    key: 'render',
     value: function render() {
       console.log(this.props);
       return React.createElement(
-        "div",
-        { className: "header" },
+        'div',
+        { className: 'header' },
         React.createElement(
-          "h1",
+          'h1',
           null,
           this.props.title
         ),
         React.createElement(
-          "h2",
+          'h2',
           null,
           this.props.subtitle
         )
@@ -55,15 +67,19 @@ var Action = function (_React$Component2) {
   }
 
   _createClass(Action, [{
-    key: "render",
+    key: 'render',
+
+    // handelPick() {
+    //   alert('hello')
+    // }
     value: function render() {
       return React.createElement(
-        "div",
-        { className: "btn-group" },
+        'div',
+        { className: 'btn-group mb-2' },
         React.createElement(
-          "a",
-          { href: "#", className: "btn btn-primary" },
-          "What should I do"
+          'button',
+          { disabled: !this.props.hasOption, onClick: this.props.handalePick, className: 'btn btn-primary' },
+          'What should I do'
         )
       );
     }
@@ -82,11 +98,30 @@ var Options = function (_React$Component3) {
   }
 
   _createClass(Options, [{
-    key: "render",
+    key: 'render',
+
+    // constructor(props) {
+    //   super(props);
+    //   this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    // }
+    // handleRemoveAll(e) {
+    //   e.preventDefault();
+    //   // alert('remove all');
+    //   console.log(this.props.options);
+    // }
     value: function render() {
       return React.createElement(
-        "div",
-        { className: "options-list" },
+        'div',
+        { className: 'options-list' },
+        React.createElement(
+          'div',
+          { className: 'btn-group' },
+          React.createElement(
+            'button',
+            { disabled: this.props.optionsIsHere, onClick: this.props.removeAllOptions, className: 'btn btn-danger' },
+            'Remove all'
+          )
+        ),
         this.props.options.map(function (option) {
           return React.createElement(Option, { key: option, optionText: option });
         })
@@ -107,13 +142,9 @@ var AddOption = function (_React$Component4) {
   }
 
   _createClass(AddOption, [{
-    key: "render",
+    key: 'render',
     value: function render() {
-      return React.createElement(
-        "div",
-        { className: "add-option-list" },
-        "Add option is here"
-      );
+      return React.createElement('div', { className: 'add-option-list' });
     }
   }]);
 
@@ -129,31 +160,279 @@ var AddOption = function (_React$Component4) {
 //   </div>
 // );
 
+var Form = function (_React$Component5) {
+  _inherits(Form, _React$Component5);
+
+  function Form(props) {
+    _classCallCheck(this, Form);
+
+    var _this5 = _possibleConstructorReturn(this, (Form.__proto__ || Object.getPrototypeOf(Form)).call(this, props));
+
+    _this5.addOptions = _this5.addOptions.bind(_this5);
+    _this5.state = {
+      error: undefined
+    };
+    return _this5;
+  }
+
+  _createClass(Form, [{
+    key: 'addOptions',
+    value: function addOptions(e) {
+      e.preventDefault();
+
+      var value = e.target.elements.option.value.trim();
+      var error = this.props.addOption(value);
+
+      this.setState(function () {
+        return {
+          error: error
+        };
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'form-wraper' },
+        React.createElement(
+          'form',
+          { onSubmit: this.addOptions },
+          this.state.error && React.createElement(
+            'p',
+            null,
+            this.state.error
+          ),
+          React.createElement('input', { type: 'text', className: 'form-control mb-2', placeholder: 'Type something', name: 'option' }),
+          React.createElement(
+            'button',
+            { className: 'btn btn-info' },
+            'Add Option'
+          )
+        )
+      );
+    }
+  }]);
+
+  return Form;
+}(React.Component);
+
+;
+
+var Counter = function (_React$Component6) {
+  _inherits(Counter, _React$Component6);
+
+  function Counter(props) {
+    _classCallCheck(this, Counter);
+
+    var _this6 = _possibleConstructorReturn(this, (Counter.__proto__ || Object.getPrototypeOf(Counter)).call(this, props));
+
+    _this6.hadnleAdd = _this6.hadnleAdd.bind(_this6);
+    _this6.handleMinus = _this6.handleMinus.bind(_this6);
+    _this6.handleReset = _this6.handleReset.bind(_this6);
+    _this6.state = {
+      count: 0
+    };
+    return _this6;
+  }
+
+  _createClass(Counter, [{
+    key: 'hadnleAdd',
+    value: function hadnleAdd() {
+      console.log('hadnleAdd');
+      this.setState(function (e) {
+        return {
+          count: e.count + 1
+        };
+      });
+    }
+  }, {
+    key: 'handleMinus',
+    value: function handleMinus() {
+      console.log('handleMinus');
+      this.setState(function (e) {
+        return {
+          count: e.count - 1
+        };
+      });
+    }
+  }, {
+    key: 'handleReset',
+    value: function handleReset() {
+      console.log('handleReset');
+      this.setState(function () {
+        return {
+          count: 0
+        };
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'counter-wraper my-3' },
+        React.createElement(
+          'h2',
+          null,
+          'Counter : ',
+          this.state.count
+        ),
+        React.createElement(
+          'div',
+          { className: 'btn-group' },
+          React.createElement(
+            'button',
+            { onClick: this.hadnleAdd, className: 'btn btn-primary' },
+            '+1'
+          ),
+          React.createElement(
+            'button',
+            { disabled: this.state.count === 0, onClick: this.handleMinus, className: 'btn btn-seconadry' },
+            '-1'
+          ),
+          React.createElement(
+            'button',
+            { onClick: this.handleReset, className: this.state.count === 0 ? 'disabled btn btn-danger' : 'btn btn-danger' },
+            'Reset'
+          )
+        )
+      );
+    }
+  }]);
+
+  return Counter;
+}(React.Component);
+
+;
+
+var Visibilty = function (_React$Component7) {
+  _inherits(Visibilty, _React$Component7);
+
+  function Visibilty(props) {
+    _classCallCheck(this, Visibilty);
+
+    var _this7 = _possibleConstructorReturn(this, (Visibilty.__proto__ || Object.getPrototypeOf(Visibilty)).call(this, props));
+
+    _this7.visibiltyToggle = _this7.visibiltyToggle.bind(_this7);
+    _this7.state = {
+      visibilty: false
+    };
+    return _this7;
+  }
+
+  _createClass(Visibilty, [{
+    key: 'visibiltyToggle',
+    value: function visibiltyToggle() {
+      // alert('working');
+      this.setState(function (e) {
+        return {
+          visibilty: !e.visibilty
+        };
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'visibilty-wraper' },
+        React.createElement(
+          'h1',
+          null,
+          'Visibilty Toggole is here'
+        ),
+        React.createElement(
+          'div',
+          null,
+          React.createElement(
+            'button',
+            { onClick: this.visibiltyToggle, className: 'btn btn-danger' },
+            this.state.visibilty === false ? 'Show More' : 'Hide Details'
+          ),
+          this.state.visibilty && React.createElement(
+            'div',
+            null,
+            'visibilty'
+          )
+        )
+      );
+    }
+  }]);
+
+  return Visibilty;
+}(React.Component);
+
+;
+
 // 027 Nesting Components
 
+var FirstApp = function (_React$Component8) {
+  _inherits(FirstApp, _React$Component8);
 
-var FirstApp = function (_React$Component5) {
-  _inherits(FirstApp, _React$Component5);
-
-  function FirstApp() {
+  function FirstApp(props) {
     _classCallCheck(this, FirstApp);
 
-    return _possibleConstructorReturn(this, (FirstApp.__proto__ || Object.getPrototypeOf(FirstApp)).apply(this, arguments));
+    var _this8 = _possibleConstructorReturn(this, (FirstApp.__proto__ || Object.getPrototypeOf(FirstApp)).call(this, props));
+
+    _this8.removeAllOptions = _this8.removeAllOptions.bind(_this8);
+    _this8.handalePick = _this8.handalePick.bind(_this8);
+    _this8.addOption = _this8.addOption.bind(_this8);
+    _this8.state = {
+      options: ['Option One', 'Option Two', 'Option Three']
+    };
+    return _this8;
   }
 
   _createClass(FirstApp, [{
-    key: "render",
+    key: 'removeAllOptions',
+    value: function removeAllOptions() {
+      this.setState(function () {
+        return {
+          options: []
+        };
+      });
+    }
+  }, {
+    key: 'handalePick',
+    value: function handalePick() {
+      // alert("test");
+      var randomNum = Math.floor(Math.random() * this.state.options.length);
+      var option = this.state.options[randomNum];
+      console.log(option);
+      alert(option);
+    }
+  }, {
+    key: 'addOption',
+    value: function addOption(option) {
+      console.log(option);
+      if (!option) {
+        return 'Add Vaild Value';
+      } else if (this.state.options.indexOf(option) > -1) {
+        return 'This value already exists';
+      }
+      this.setState(function (e) {
+        return {
+          options: e.options.concat([option])
+        };
+      });
+    }
+  }, {
+    key: 'render',
     value: function render() {
       var title = "My First React App",
-          subtitle = "Some text here",
-          options = ['Option One', 'Option Two', 'Option Three'];
+          subtitle = "Some text here";
+      // options = ['Option One', 'Option Two', 'Option Three'];
       return React.createElement(
-        "div",
-        { className: "col-md-8 mx-auto text-center" },
+        'div',
+        { className: 'col-md-8 mx-auto text-center' },
         React.createElement(Header, { title: title, subtitle: subtitle }),
-        React.createElement(Action, null),
-        React.createElement(Options, { options: options }),
-        React.createElement(AddOption, null)
+        React.createElement(Action, { hasOption: this.state.options.length > 0, handalePick: this.handalePick }),
+        React.createElement(Options, { options: this.state.options, optionsIsHere: this.state.options.length === 0, removeAllOptions: this.removeAllOptions }),
+        React.createElement(AddOption, null),
+        React.createElement(Form, { addOption: this.addOption }),
+        React.createElement(Counter, null),
+        React.createElement(Visibilty, null)
       );
     }
   }]);
@@ -161,8 +440,8 @@ var FirstApp = function (_React$Component5) {
   return FirstApp;
 }(React.Component);
 
-var Option = function (_React$Component6) {
-  _inherits(Option, _React$Component6);
+var Option = function (_React$Component9) {
+  _inherits(Option, _React$Component9);
 
   function Option() {
     _classCallCheck(this, Option);
@@ -171,11 +450,11 @@ var Option = function (_React$Component6) {
   }
 
   _createClass(Option, [{
-    key: "render",
+    key: 'render',
     value: function render() {
       return React.createElement(
-        "div",
-        { className: "option-list" },
+        'div',
+        { className: 'option-list' },
         this.props.optionText
       );
     }
--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 console.log('app js is running');
 
+let obj = {
+  name: 'Vikram',
+  getName() {
+    return this.name;
+  }
+}
+
+// let getName = obj.getName.bind(obj);
+let getName = obj.getName.bind({name: 'ABC'});
+
+console.log(getName())
+
 // 026 Creating a React Component
 
 class Header extends React.Component {
@@ -15,16 +27,28 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  // handelPick() {
+  //   alert('hello')
+  // }
   render () {
     return (
-      <div className="btn-group">
-        <a href="#" className="btn btn-primary">What should I do</a>
+      <div className="btn-group mb-2">
+        <button disabled={!this.props.hasOption} onClick={this.props.handalePick} className="btn btn-primary">What should I do</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.handleRemoveAll = this.handleRemoveAll.bind(this);
+  // }
+  // handleRemoveAll(e) {
+  //   e.preventDefault();
+  //   // alert('remove all');
+  //   console.log(this.props.options);
+  // }
   render() {
     return (
       <div className="options-list">
@@ -33,6 +57,10 @@ class Options extends React.Component {
             return <p className="text-danger" key={option}>{option}</p>
           })}
         </div> */}
+        <div className="btn-group">
+          {/* <a href="#" onClick={this.handleRemoveAll.bind(this)} className="btn btn-danger">Remove all</a> */}
+          <button disabled={this.props.optionsIsHere} onClick={this.props.removeAllOptions} className="btn btn-danger">Remove all</button>
+        </div>
         {
           this.props.options.map(option => {
             return <Option key={option} optionText={option} />
@@ -47,9 +75,7 @@ class Options extends React.Component {
 class AddOption extends React.Component {
   render() {
     return (
-      <div className="add-option-list">
-        Add option is here
-      </div>
+      <div className="add-option-list"></div>
     );
   }
 }
@@ -63,18 +89,171 @@ class AddOption extends React.Component {
 //   </div>
 // );
 
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addOptions = this.addOptions.bind(this);
+    this.state = {
+      error: undefined
+    }
+  }
+  addOptions (e) {
+    e.preventDefault();
+    
+    let value = e.target.elements.option.value.trim();
+    let error = this.props.addOption(value);
+
+    this.setState(() => {
+      return {
+        error: error
+      }
+    })
+  }
+  render () {
+    return (
+      <div className="form-wraper">
+        <form onSubmit={this.addOptions}>
+          {this.state.error && <p>{this.state.error}</p>}
+          <input type="text" className="form-control mb-2" placeholder="Type something" name="option" />
+          <button className="btn btn-info">Add Option</button>
+        </form>
+      </div>
+    );
+  }
+};
+
+class Counter extends React.Component {
+  constructor (props) {
+    super(props);
+    this.hadnleAdd = this.hadnleAdd.bind(this);
+    this.handleMinus = this.handleMinus.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.state = {
+      count: 0
+    }
+  }
+  hadnleAdd() {
+    console.log('hadnleAdd');
+    this.setState((e) => {
+      return {
+        count: e.count + 1
+      };
+    })
+  }
+  handleMinus() {
+    console.log('handleMinus');
+    this.setState((e) => {
+      return {
+        count: e.count - 1
+      }
+    })
+  }
+  handleReset() {
+    console.log('handleReset');
+    this.setState(() => {
+      return {
+        count: 0
+      };
+    })
+  }
+  render() {
+    return (
+      <div className="counter-wraper my-3">
+        <h2>Counter : {this.state.count}</h2>
+        <div className="btn-group">
+          <button onClick={this.hadnleAdd} className="btn btn-primary">+1</button>
+          <button disabled={this.state.count === 0} onClick={this.handleMinus} className="btn btn-seconadry">-1</button>
+          <button onClick={this.handleReset} className={this.state.count === 0 ? 'disabled btn btn-danger': 'btn btn-danger'}>Reset</button>
+        </div>
+      </div>
+    );
+  }
+};
+
+class Visibilty extends React.Component {
+  constructor(props) {
+    super(props);
+    this.visibiltyToggle = this.visibiltyToggle.bind(this);
+    this.state = {
+      visibilty: false
+    }
+  }
+  visibiltyToggle() {
+    // alert('working');
+    this.setState((e) => {
+      return {
+        visibilty: !e.visibilty
+      };
+    })
+  }
+  render() {
+    return (
+      <div className="visibilty-wraper">
+        <h1>Visibilty Toggole is here</h1>
+        <div>
+          <button onClick={this.visibiltyToggle} className="btn btn-danger">{this.state.visibilty === false ? 'Show More': 'Hide Details'}</button>
+          {
+            this.state.visibilty && (
+              <div>visibilty</div>
+            )
+          }
+        </div>
+      </div>
+    );
+  }
+};
+
 // 027 Nesting Components
 class FirstApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.removeAllOptions = this.removeAllOptions.bind(this);
+    this.handalePick = this.handalePick.bind(this);
+    this.addOption = this.addOption.bind(this);
+    this.state = {
+        options:  ['Option One', 'Option Two', 'Option Three']
+    }
+  }
+  removeAllOptions() {
+    this.setState(() => {
+      return {
+        options: []
+      };
+    })
+  }
+  handalePick() {
+    // alert("test");
+    let randomNum = Math.floor(Math.random() * this.state.options.length);
+    let option = this.state.options[randomNum];
+    console.log(option);
+    alert(option);
+  }
+  addOption(option) {
+    console.log(option);
+    if (!option) {
+      return 'Add Vaild Value'
+    } else if(this.state.options.indexOf(option) > -1) {
+      return 'This value already exists'
+    }
+    this.setState ((e) => {
+      return {
+        options: e.options.concat([option])
+      };
+    })
+  }
   render () {
     let title = "My First React App",
-        subtitle = "Some text here",
-        options = ['Option One', 'Option Two', 'Option Three'];
+        subtitle = "Some text here";
+        // options = ['Option One', 'Option Two', 'Option Three'];
     return (
       <div className="col-md-8 mx-auto text-center">
         <Header title={title} subtitle={subtitle}/>
-        <Action />
-        <Options options={options}/>
+        <Action hasOption={this.state.options.length > 0} handalePick={this.handalePick} />
+        <Options options={this.state.options} optionsIsHere={this.state.options.length === 0} removeAllOptions={this.removeAllOptions}/>
         <AddOption />
+        <Form addOption={this.addOption}/>
+        <Counter />
+        <Visibilty />
       </div>
     );
   }
